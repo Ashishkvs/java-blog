fdad12c5f4d62fdaf9a5b4698f000093d2c0d820
Ashish Yadhuvanshi <ashishkvs@gmail.com>
2020-02-01 16:53:28 +0530

    updated basic security

--- src/main/java/com/imagegrafia/blog/config/WebSecurityConfig.java
+++ src/main/java/com/imagegrafia/blog/config/WebSecurityConfig.java
@@ -17,10 +17,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-//		http.authorizeRequests().antMatchers("/api").hasAnyRole("ADMIN");
-//		http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
-//		http.csrf().disable().antMatcher("/h2-console/");
-		//secure all http request
 		http.httpBasic().and().authorizeRequests()
 		.antMatchers("/h2-console**").permitAll()
 		.antMatchers("/api/**").hasAnyRole("ADMIN")
